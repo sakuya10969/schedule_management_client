2c744f1e5b407a76f065a249be9f32c616c8428b
sakuya10969 <sakuya.f20010618@gmail.com>
2025-05-07 17:29:16 +0900

    appointment api logic

--- features/appointment/api.ts
+++ features/appointment/api.ts
@@ -12,10 +12,10 @@ interface FormData {
 
 export const fetchFormData = async (apiUrl: string, token: string): Promise<FormData> => {
   try {
-    const response = await axios.get(`${apiUrl}/retrieve_form_data`, {
+    const { data } = await axios.get(`${apiUrl}/retrieve_form_data`, {
       params: { token },
     });
-    return response.data;
+    return data;
   } catch (error) {
     console.error("Error retrieving form data:", error);
     throw new Error("データの取得に失敗しました。");
@@ -24,8 +24,8 @@ export const fetchFormData = async (apiUrl: string, token: string): Promise<Form
 
 export const submitSchedule = async (apiUrl: string, payload: any) => {
   try {
-    const response = await axios.post(`${apiUrl}/appointment`, payload);
-    return response.data;
+    const { data } = await axios.post(`${apiUrl}/appointment`, payload);
+    return data;
   } catch (error) {
     console.error("日程の確定に失敗しました:", error);
     throw new Error("日程の確定に失敗しました。");
