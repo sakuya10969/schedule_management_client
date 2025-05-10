a73d18e0716bdb0c81db620660b4dc8210035e18
sakuya10969 <sakuya.f20010618@gmail.com>
2025-05-10 16:14:37 +0900

    fix: weekdays

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,5 @@
 // export const weekdays = ['月', '火', '水', '木', '金'];
 
-export const weekdays = ['月', '火', '水', '木', '金', '土', '日'];
+export const weekdays = ['日', '月', '火', '水', '木', '金', '土'];
 
 export const durationOptions = [30, 60, 90, 120];
