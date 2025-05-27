a3fc7cc4ec3376dbb7e9d273bac9859499f56b06
sakuya10969 <sakuya.f20010618@gmail.com>
2025-05-27 17:06:23 +0900

    feat: redirect schedule page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,6 @@
 // app/page.tsx
-import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold mb-4">ホームページ</h1>
-      <Link
-        href="/schedule"
-        className="inline-block bg-gray-500 text-white px-4 py-2 rounded "
-      >
-        スケジュールページへ
-      </Link>
-    </main>
-  );
+  redirect('/schedule');
 }
