872e68aee5755e45d8ce7f9d75d8a342e43c50f9
sakuya10969 <sakuya.f20010618@gmail.com>
2025-09-26 14:19:42 +0900

    feat: shadcn checkbox for weekdays selector

--- app/schedule/components/WeekdaySelector.tsx
+++ app/schedule/components/WeekdaySelector.tsx
@@ -1,37 +1,37 @@
 import { weekdays } from '@/constants';
 import { handleDayToggle } from '@/features/schedule/utils';
 import { WeekdaySelectorProps } from '@/features/schedule/types';
+import { Button, Checkbox } from '@/components/ui';
 
 const WeekdaySelector = ({
   selectedDays,
   setSelectedDays,
 }: WeekdaySelectorProps) => {
   return (
-    <div className="p-4 rounded-lg">
+    <div className="p-3 rounded-lg">
       <label className="block mb-1 font-bold text-xl text-black">
         曜日選択
       </label>
       <div className="flex flex-wrap gap-4">
         {weekdays.map((day) => (
-          <label key={day} className="flex items-center space-x-2">
-            <input
-              type="checkbox"
+          <div key={day} className="flex items-center space-x-2">
+            <Checkbox
               checked={selectedDays.includes(day)}
-              onChange={() =>
+              onCheckedChange={() =>
                 handleDayToggle(day, selectedDays, setSelectedDays)
               }
               className="w-5 h-6 cursor-pointer"
             />
             <span className="text-xl font-medium text-black">{day}</span>
-          </label>
+          </div>
         ))}
-        <button
+        <Button
           type="button"
           onClick={() => setSelectedDays([])}
-          className="bg-blue-300 hover:bg-blue-400 text-sm text-black py-1 px-2 rounded"
+          className="bg-blue-100 hover:bg-blue-200 text-md text-black px-3 py-2 rounded"
         >
           曜日選択をリセット
-        </button>
+        </Button>
       </div>
     </div>
   );
